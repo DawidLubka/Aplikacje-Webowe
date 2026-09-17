91b7b7dfc7b4d9abf47e82bb6c4edb19556ad28c
Dawid <dawidlubka9@gmail.com>
2026-09-17 11:39:32 +0200

    navbar

---
+++ mb05/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+
+function App() {
+  return(
+    <>
+      <Navbar />
+      <div className="container mt-4">
+        <h1>Galeria Zdjęć</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
